58b73ec8b746f37d654aebe71bd61cc7a37cfea6
Rhonda Malecki <rcmalecki@gmail.com>
2016-05-08 22:46:03 -0500

    Week 15 - Round One of Final Revisions

---
+++ week_15/northernmoonsite_RD/PUBLIC/js/main.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+
+	console.log("This is the file main.js speaking.");
+
+
+	$("section#portfolio article").hover(function () {
+
+		overlayToggle($(this));
+
+	});
+
+	function overlayToggle(articleHovered) {
+
+		console.log("This is the function overlayToggle() speaking.");
+
+		articleHovered.children("div.overlay").stop().slideToggle();
+
+	}
+
+	cssFixes();
+
+	function cssFixes() {
+
+		var articleHeight = $("section#portfolio article:first-of-type").outerHeight();
+		console.log(articleHeight);
+
+		$("section#portfolio article div.overlay").outerHeight(articleHeight);
+
+
+	}
+
+	$(window).resize(function() {
+
+		cssFixes();
+
+	});
+
+
+});
+
+	jQuery(function($){
+    	     $( '.menu-btn' ).click(function(){
+    	     $('.responsive-menu').toggleClass('expand')
+    	     })
+        })
+
+	$(document).ready(function(){
+		$("#products").each(function(e) {
+			if (e != 0)
+				$(this).hide();
+		});
+
+		$("#arrow-right").click(function(){
+			if ($("#products article:visible").next().length != 0)
+				$("#products article:visible").next().show().prev().hide();
+			else {
+				$("#products article:visible").hide();
+				$("#products article:first").show();
+			}
+			return false;
+		});
+
+		$("#arrow-left").click(function(){
+			if ($("#products article:visible").prev().length != 0)
+				$("#products article:visible").prev().show().next().hide();
+			else {
+				$("#products article:visible").hide();
+				$("#products article:last").show();
+			}
+			return false;
+		});
+		
+	});
+
+
