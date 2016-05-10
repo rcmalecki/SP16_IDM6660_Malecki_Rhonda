3555124f68af852740c1513d5baa99f80de7f2dc
Rhonda Malecki <rcmalecki@gmail.com>
2016-05-10 14:57:15 -0500

    week 15 - round two of final revisions

---
+++ week_15/northernmoonsite_FINAL/PUBLIC/pages/contact-form.php
@@ -0,0 +1,14 @@
+<?php 
+				$name=$_REQUEST['name']; 
+				$email=$_REQUEST['email']; 
+				$message=$_REQUEST['message']; 
+				if (($name=="")||($email=="")||($message=="")) 
+					{ 
+					echo "All fields are required. Please fill out <a href='../pages/contact.html'>the form</a> again."; 
+					} 
+				else{         
+					$from="From: $name<$email>\r\nReturn-path: $email"; 
+					$subject="Message sent using your contact form"; 
+					mail("rcmalecki@gmail.com", $subject, $message, $from) or die("Error!"); 
+					echo 'Email sent! Return to <a href="../pages/contact.html">the site</a> again.'; 
+					} 
